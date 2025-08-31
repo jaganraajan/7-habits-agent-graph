b6e01fe4ca3eb84b6d21f172b24af4163e69c429
jaganraajan <jaganraajan@gmail.com>
2025-08-30 20:16:23 -0400

    fix linear chat completion from azure openai

--- graphs/01-linear/01_linear.py
+++ graphs/01-linear/01_linear.py
@@ -1,8 +1,10 @@
+import os
+from dotenv import load_dotenv
 from typing import Annotated, TypedDict
 
 from langchain_core.messages import AIMessage, BaseMessage, SystemMessage
 from langchain_core.runnables import RunnableConfig
-from langchain_openai import ChatOpenAI
+from langchain_openai import AzureChatOpenAI
 from langgraph.graph import END, START, StateGraph
 from langgraph.graph.message import add_messages
 
@@ -23,16 +25,24 @@ def init_state() -> State:
 
 @registered_graph("01-linear")
 def build_graph() -> StateGraph:
+    load_dotenv()
     
     def chat_node(state: State, config: RunnableConfig) -> State:
+        subscription_key = os.getenv("AZURE_OPENAI_API_KEY")
+        api_version = "2024-12-01-preview"
+
         # Initialize LLM
-        # llm = ChatOpenAI(model="gpt-4o-mini")
-        print('in chat node')
-        # Invoke LLM with all messages from state (add_messages handles history)
-        ai = AIMessage(content="This is a demo response from the AI.")
+        llm = AzureChatOpenAI(
+            api_key=subscription_key,
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            api_version=api_version,
+            deployment_name="gpt-4o-mini"
+        )
+        # # Invoke LLM with all messages from state (add_messages handles history)
+        ai: AIMessage = llm.invoke(state["messages"], config=config)
+
+        # Return only the AI response; add_messages will append it
         return {"messages": [ai]}
-
-
     # initialize graph
     graph = StateGraph(State)
 
